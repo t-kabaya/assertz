a909422a99c44192ec1e2fe6e858155dce9b1120
soniczsonic <kabaya.dori@gmail.com>
2019-10-11 21:12:40 +0900

    feature: change fixture filenames

--- testRunner.js
+++ testRunner.js
@@ -1,35 +1,33 @@
-// const fs = require('fs')
-
 // // __dirnameは本番環境では使えないので、../../などに変える。
 // const fileNames = fs.readdirSync(__dirname)
 
-// console.log(fileNames)
-
 const executeTest = file => {
   // inheritがキモ。consoleの出力を共有出来る。
-  console.log('executeTest' + file)
+  // console.log('executeTest' + file)
   if (file && file.endsWith('.test.js')) {
     require('child_process').execSync(`node ${file}`, { stdio: 'inherit' })
   }
 }
 
 const fs = require('fs')
-const walk = dir => {
+const runTest = dir => {
   const fileNames = fs.readdirSync(dir)
 
   // folders = fileNames.filter(file => )
-  jsFiles = fileNames.filter(f => f.endsWith('.js'))
-  console.log({ jsFiles })
+  const jsFiles = fileNames.filter(f => f.endsWith('.js'))
+  // console.log({ jsFiles })
   jsFiles.forEach(file => executeTest('/' + dir + '/' + file))
   executeTest()
 
   // gitとnode_modulesを取り除く。
-  projectFileNames = fileNames.filter(f => f !== '.git' && f !== 'node_modules')
+  const projectFileNames = fileNames.filter(
+    f => f !== '.git' && f !== 'node_modules'
+  )
 
   if (!projectFileNames) return
-  console.log({ projectFileNames })
+  // console.log({ projectFileNames })
 
-  directories = projectFileNames.filter(f => {
+  const directories = projectFileNames.filter(f => {
     const stat = fs.statSync(dir + '/' + f)
     if (stat) {
       return stat.isDirectory()
@@ -37,31 +35,22 @@ const walk = dir => {
       return false
     }
   })
-  console.log({ directories })
-  // console.log({ directories })
-  // const childDir = dir + '/' + directories[0]
-  // console.log({ childDir })
-  // console.log({ dir })
-  // if (childDir) {
-  //   console.log(fs.statSync(childDir).isDirectory())
-  //   console.log(fs.readdirSync(childDir))
-  // }
 
   directories.map(childDir => {
-    console.log({ childDir })
-    walk(dir + '/' + childDir)
+    // console.log({ childDir })
+    runTest(dir + '/' + childDir)
   })
 
   // fileNames.forEach(fileName => {
   //   const stat = fs.statSync(fileName)
   //   console.log(stat.isDirectory())
   //   if (stat.isDirectory) {
-  //     walk(dir + '/' + fileName)
+  //     runTest(dir + '/' + fileName)
   //   }
   //   // console.log(fileName)
   // })
 }
 
-const result = walk(__dirname)
+const result = runTest(__dirname)
 
 // 完成した。
