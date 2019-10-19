2cc8b0dc8dbba7885162cc35c8e2546b87a16795
t-kabaya <ipg@IPGnoMacBook-puro.local>
2019-10-19 16:25:28 +0900

    feature: [has bug] findTests return array

--- testRunner.js
+++ testRunner.js
@@ -3,35 +3,51 @@ const { execSync } = require('child_process')
 
 const testFileNamePattern = '.test.js'
 
-const execTest = (path, fileName) => {
+const execTests = (testFiles, dir) => {
   // TODO: fileNameを、テスト結果に載せる。
   // assertzのassertは、即時関数なので、テストファイルを、requireするだけで実行される。
   require(path)
 }
 
+const foundedTests = []
+
 // TODO: findTestsの中で、execTestを実行するのは、関数型らしくない。
 // findTestsで、pathの、配列を作成してから、それを帰り値として、returnし、execTestはそれを引数として取得するようにリファクタリングする。
 const findTests = dir => {
-  const fileNames = fs.readdirSync(dir)
+  const result = []
 
-  const testFiles = fileNames.filter(f => f.endsWith(testFileNamePattern))
-  testFiles.forEach(file => execTest('/' + dir + '/' + file, file))
+  const walk = dir => {
+    const fileNames = fs.readdirSync(dir)
 
-  // gitと、node_modulesを除外
-  const projectFileNames = fileNames.filter(
-    f => f !== '.git' && f !== 'node_modules'
-  )
+    // fileNames.forEach()
 
-  if (!projectFileNames) return
+    const testFiles = fileNames.filter(f => f.endsWith(testFileNamePattern))
+    const tmpResult = testFiles.map(file => '/' + dir + '/' + file)
+    // result.push(tmpResult)
+    tmpResult.forEach(files => {
+      result.push(files)
+    })
 
-  const directories = projectFileNames.filter(f => {
-    const stat = fs.statSync(dir + '/' + f)
-    if (!stat) return false
+    // gitと、node_modulesを除外
+    const projectFileNames = fileNames.filter(
+      f => f !== '.git' && f !== 'node_modules'
+    )
 
-    return stat.isDirectory()
-  })
+    if (!projectFileNames) return
 
-  directories.map(childDir => findTests(dir + '/' + childDir))
+    const directories = projectFileNames.filter(f => {
+      const stat = fs.statSync(dir + '/' + f)
+      if (!stat) return false
+
+      return stat.isDirectory()
+    })
+
+    directories.map(childDir => walk(dir + '/' + childDir))
+  }
+
+  walk(dir)
+
+  return result
 }
 
 module.exports = { findTests }
