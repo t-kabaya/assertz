1d7fbfda28d40188c33930c4c53515d6add803b0
t-kabaya <ipg@IPGnoMacBook-puro.local>
2019-10-24 11:46:20 +0900

    build: add env in cli.js

--- cli.js
+++ cli.js
@@ -2,5 +2,10 @@ const { findTests, runTests } = require('./testRunner')
 const showTestResult = require('./showTestResult')
 const { store } = require('./store')
 
-const paths = findTests(__dirname.replace('/node_modules/assertz', ''))
+const env = process.env.ENV
+const root =
+  env === 'development'
+    ? __dirname
+    : __dirname.replace('/node_modules/assertz', '')
+const paths = findTests(root)
 runTests(paths)
