16ef9414781f9c42d1c7ab36e334714eeeeb3e5f
t-kabaya <ipg@IPGnoMacBook-puro.local>
2019-11-03 23:08:25 +0900

    fix: fix wrong ROOT_FOLDER error

--- env.ts
+++ env.ts
@@ -1,4 +1,4 @@
 export const NODE_ENV: String = process.env.NODE_ENV || ''
 export const ROOT_FOLDER = NODE_ENV === 'development'
   ? __dirname
-  : __dirname.replace('/node_modules/assertz', '')
+  : __dirname.replace('/node_modules/assertz/dist', '')
